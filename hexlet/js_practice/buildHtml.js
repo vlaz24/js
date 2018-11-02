fdac0465e40fc1a59b1126557c703c4590525c7e
Vladislav Lazarevits <v.lazarevits@gmail.com>
2018-11-02 09:16:55 +0200

    Update hexlet/js_practice/buildHtml.js
    js practice

---
+++ hexlet/js_practice/buildHtml.js
@@ -0,0 +1,44 @@
+export default () => table(
+    tr(td('lang'), td('comment')),
+    tr(td('php'), td('statements')),
+    tr(td('clojure'), td('expressions')),
+  );
+
+  /*Модуль tags содержит набор функций, позволяющих генерировать HTML с помощью функций.
+
+table(
+    tr(td('one'), td('two')),
+    tr(td('three'), td('four'))
+  );
+  В результате вызовов возвращается вот такой HTML:
+  
+  <table>
+    <tr>
+      <td>one</td>
+      <td>two</td>
+    </tr>
+    <tr>
+      <td>three</td>
+      <td>four</td>
+    </tr>
+  </table>
+  buildHtml.js
+  Реализуйте и экспортируйте по умолчанию функцию buildHtml, простой вызов которой:
+  
+  buildHtml();
+  создаёт и возвращает следующий HTML:
+  
+  <table>
+    <tr>
+      <td>lang</td>
+      <td>comment</td>
+    </tr>
+    <tr>
+      <td>php</td>
+      <td>statements</td>
+    </tr>
+    <tr>
+      <td>clojure</td>
+      <td>expressions</td>
+    </tr>
+  </table>
